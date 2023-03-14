a40c4ab325c912d46b46775fb6d9f6385ef8ef75
Masum Didar <masumdidar.cse@gmail.com>
2023-03-14 22:23:52 +0600

    initial commit

---
+++ first.js
@@ -0,0 +1,5 @@
+
+document.write("<h1> This is External JavaScript</h1>");
+document.write("<h4>Name: Abu Masum Didar</h4>");
+document.write("<h5>Jr. Software Engineer (RoR)</h5>");
+document.write("<h6>Syftet Limited</h6>");
